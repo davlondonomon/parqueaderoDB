e9ea85281d477f5971848e99d43a9e267c776175
davlondonomon <davlondonomon@unal.edu.co>
2019-08-16 00:15:40 +0200

    a

--- parqueadero/configuraciones/conexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-$host = "localhost";
-$user = "parqueadero";
-$pass = "clave";
-$DB = "parqueadero";
-$conn = mysqli_connect($host, $user, $pass, $DB) or die("Error al conectar a la DB " . mysqli_error($link));
-?>
